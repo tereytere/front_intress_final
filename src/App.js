580e0bbe4f37ded85e538f165db4e5b369c84bfa
kiaramm10 <kiaramalasquez5@gmail.com>
2023-04-17 19:40:48 +0200

    Create new update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Holidays from './components/holidays/Holidays';
 import Signin from './components/signin/Signin';
+import Login from './pages/login/Login';
+import NewUser from './components/create/NewUser';
+import NewWorkshop from './components/create/NewWorkshop';
 
 function App() {
   return (
     <div className="App">
-      <Signin/>
-      <Holidays/>
+      <NewUser />
+      <NewWorkshop />
+
     </div>
   );
 }
