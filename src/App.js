d20a15a5cf6b30fdf8c20c6df84dc91ebd51e09b
Teresa <teresa.ambroa@hotmail.com>
2023-04-20 10:21:21 +0200

    subir documento

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import './App.css';
-import Holidays from './components/holidays/Holidays';
-import Signin from './components/signin/Signin';
-import Login from './pages/login/Login';
-import NewUser from './components/create/NewUser';
-import NewWorkshop from './components/create/NewWorkshop';
+import React, { useState } from "react";
+import Documents from "./components/documents/Documents";
 
 function App() {
+  const [newUserInfo, setNewUserInfo] = useState({
+    profileImages: []
+  });
+
+  const updateUploadedFiles = (files) =>
+    setNewUserInfo({ ...newUserInfo, profileImages: files });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //logic to create new user...
+  };
+
   return (
-    <div className="App">
-      <Holidays/>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Documents
+          accept=".jpg,.png,.jpeg,.pdf"
+          multiple
+          updateFilesCb={updateUploadedFiles}
+          apiEndpoint="http://127.0.0.1:8000/apidocuments/create"
+        />
+      </form>
     </div>
   );
 }
