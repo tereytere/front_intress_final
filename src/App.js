c7732dfb40bb6fd12f6aa3893a4a724877102c12
Teresa <teresa.ambroa@hotmail.com>
2023-04-20 10:49:51 +0200

    doc

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import React, { useState } from "react";
-import Documents from "./components/documents/Documents";
+import Doc from "./components/documents/Doc";
 
 function App() {
-  const [newUserInfo, setNewUserInfo] = useState({
-    profileImages: []
-  });
-
-  const updateUploadedFiles = (files) =>
-    setNewUserInfo({ ...newUserInfo, profileImages: files });
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    //logic to create new user...
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <Documents
-          accept=".jpg,.png,.jpeg,.pdf"
-          multiple
-          updateFilesCb={updateUploadedFiles}
-          apiEndpoint="http://127.0.0.1:8000/apidocuments/create"
-        />
-      </form>
-    </div>
-  );
+  <Doc/>
 }
 
 export default App;
