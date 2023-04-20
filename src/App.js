534dca6f855f0d4d57a8888793428f425b8e282e
Teresa <teresa.ambroa@hotmail.com>
2023-04-20 10:55:31 +0200

    docs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import Doc from './components/documents/Doc';
 import SelecUsuario from './components/selecusuario/SelecUsuario';
-function App() {
-return (
-<div className="App">
 
-<SelecUsuario />
-</div>
-);
+function App() {
+    return (
+        <div className="App">
+
+            <Doc/>
+            <SelecUsuario />
+        </div>
+    );
 }
 
 export default App;
