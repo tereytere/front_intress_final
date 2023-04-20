c0316d7bea584917a709fd51e2ea9ff47e168fbc
milarte1 <116848503+milarte1@users.noreply.github.com>
2023-04-20 10:01:56 +0200

    selecAdmin

---
+++ src/components/selecusuario/selecAdmin/SelecAdmin.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './selecAdmin.css';
+function SelecAdmin () {
+    return (
+        <div className="container-selec">
+            <div className="selec-admin">
+                <select>
+                <option value="opcion1">USUARIOS</option>
+                <option value="opcion2">TALLERES</option>
+                <option value="opcion3">TAREAS</option>
+                </select>
+
+    </div>
+    </div>
+);
+}
+
+export default SelecAdmin;
