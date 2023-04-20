faa2bdc4561bf1b73f1569309990604fbcd678cb
Teresa <teresa.ambroa@hotmail.com>
2023-04-20 14:52:31 +0200

    downloads

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Doc from './components/documents/Doc';
+import Downloads from './components/downloads/Downloads';
 import SelecUsuario from './components/selecusuario/SelecUsuario';
 
 function App() {
     return (
         <div className="App">
-
+            <Downloads/>
             <Doc/>
             <SelecUsuario />
         </div>
