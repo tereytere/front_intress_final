a477b44fada591568b012d39e44c91216013d725
milarte1 <116848503+milarte1@users.noreply.github.com>
2023-04-21 22:09:24 +0200

    excelbuttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import SelecAdmin from './components/selecusuario/selecAdmin/SelecAdmin';
+import DocumentsEscel from './components/exceldownloadbutton/DocumentsEscel';
 import ExcelDownloadButton from './components/exceldownloadbutton/ExcelDownloadButton';
+import ExcelHolidays from './components/exceldownloadbutton/ExcelHolidays';
+import WorkshopsEscel from './components/exceldownloadbutton/WorkshopsEscel';
 function App() {
 return (
 <div className="App">
 <ExcelDownloadButton />
-
+<ExcelHolidays />
+<DocumentsEscel />
+<WorkshopsEscel />
 </div>
 );
 }
