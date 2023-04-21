9d6da6baf4781ec88f51bfdb0b92aebf00ea538c
milarte1 <116848503+milarte1@users.noreply.github.com>
2023-04-21 11:23:29 +0200

    excellpruebaupdate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import SelecAdmin from './components/selecusuario/selecAdmin/SelecAdmin';
+import ExcelDownloadButton from './components/exceldownloadbutton/ExcelDownloadButton';
 function App() {
 return (
 <div className="App">
-
+<ExcelDownloadButton />
 <SelecAdmin />
 </div>
 );
