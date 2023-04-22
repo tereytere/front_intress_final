165b008558ed8ae8ae62159a8d8b33961a79b3d3
kiaramm10 <kiaramalasquez5@gmail.com>
2023-04-22 23:11:52 +0200

    update

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -4,16 +4,18 @@ import SelecAdmin from '../../components/selecusuario/selecAdmin/SelecAdmin'
 import NewUser from '../../components/create/NewUser'
 import NewTask from '../../components/create/NewTask'
 import NewWorkshop from '../../components/create/NewWorkshop'
+import Personal from '../../components/personal/Personal'
 
 function Admin() {
   return (
     <div>
       <NavbarAdmin />
       <Personal />
-      <SelecAdmin/>
+      <SelecAdmin />
       <NewUser />
       <NewTask />
       <NewWorkshop />
+      
     </div>
   )
 }
